0e53023ce92bd25a32eb8b5f602f3d8c3215c748
Julianna Fernandes <juliannabf29@gmail.com>
2025-02-24 19:45:53 -0300

    feat: criando model Cliente

---
+++ app/Database/Migrations/2025-02-24-212437_Cliente.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Cliente extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nome_razao_social' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => false,
+            ],
+            'cpf_cnpj' => [
+                'type' => 'VARCHAR',
+                'constraint' => '20',
+                'null' => false,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'update_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('cliente');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('cliente');
+    }
+}
---
+++ app/Models/Cliente.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Cliente extends Model
+{
+    protected $table            = 'cliente';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'nome_razao_social',
+        'cpf_cnpj',
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+
+}
