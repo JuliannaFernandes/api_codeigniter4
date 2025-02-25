8c7bba206e8bc514d8ea9871d6a6ceb4f81516e4
Julianna Fernandes <juliannabf29@gmail.com>
2025-02-24 23:43:38 -0300

    feat: criando método de listar produto

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,8 @@ $routes->get('cliente', 'ClienteController::index');
 $routes->post('cliente', 'ClienteController::create');
 $routes->put('cliente/(:num)', 'ClienteController::update/$1');
 $routes->delete('cliente/(:num)', 'ClienteController::delete/$1');
+
+$routes->get('produto', 'ProdutoController::index');
+
+
+
