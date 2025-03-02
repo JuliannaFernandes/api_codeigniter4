1d2c7d2fcbb4e25ced448bd7d0c9d2e94963b16e
Julianna Fernandes <juliannabf29@gmail.com>
2025-03-02 19:22:52 -0300

    feat: adicionando proteção JWT nos endpoints

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,19 +8,19 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->get('cliente', 'ClienteController::index', ['filter' => 'authFilter']);
-$routes->post('cliente', 'ClienteController::create');
-$routes->put('cliente/(:num)', 'ClienteController::update/$1');
-$routes->delete('cliente/(:num)', 'ClienteController::delete/$1');
+$routes->post('cliente', 'ClienteController::create', ['filter' => 'authFilter']);
+$routes->put('cliente/(:num)', 'ClienteController::update/$1', ['filter' => 'authFilter']);
+$routes->delete('cliente/(:num)', 'ClienteController::delete/$1', ['filter' => 'authFilter']);
 
 $routes->get('produto', 'ProdutoController::index', ['filter' => 'authFilter']);
-$routes->post('produto', 'ProdutoController::create');
-$routes->put('produto/(:num)', 'ProdutoController::update/$1');
-$routes->delete('produto/(:num)', 'ProdutoController::delete/$1');
+$routes->post('produto', 'ProdutoController::create', ['filter' => 'authFilter']);
+$routes->put('produto/(:num)', 'ProdutoController::update/$1', ['filter' => 'authFilter']);
+$routes->delete('produto/(:num)', 'ProdutoController::delete/$1', ['filter' => 'authFilter']);
 
 $routes->get('pedido', 'PedidoController::index', ['filter' => 'authFilter']);
-$routes->post('pedido', 'PedidoController::create');
-$routes->put('pedido/(:num)', 'PedidoController::update/$1');
-$routes->delete('pedido/(:num)', 'PedidoController::delete/$1');
+$routes->post('pedido', 'PedidoController::create', ['filter' => 'authFilter']);
+$routes->put('pedido/(:num)', 'PedidoController::update/$1' , ['filter' => 'authFilter']);
+$routes->delete('pedido/(:num)', 'PedidoController::delete/$1', ['filter' => 'authFilter']);
 
 $routes->post("registro", "RegistroController::index");
 $routes->post("login", "LoginController::index");
